e51081955cd57809d65af49e7d2aebcbc5c99653
Marie Oestreich <marie.oestreich@dzne.de>
2025-05-16 13:49:19 +0200

    filled in main with adapted answers

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "Paul Käufl",
+    name: "Marie Oestreich",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["red, green and yellow", "fast feedback", "iterative development process"],
+    associations: ["not much yet, only what i have heard in the Onboarding"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins",
+    tools: "",
     // What do you want to learn in this workshop?
-    expectations: ["How to improve the workshop.", "Which CI/CD Tools do you use in your projects?"],
+    expectations: ["Basics of CI/CD"],
   },
 });
